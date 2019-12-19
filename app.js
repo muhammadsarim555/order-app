67765daef8f1206dadc2d8d8a8ce62edb14415f7
muhammadsarim555 <muhammadsarim555@gmail.com>
2019-12-19 12:36:47 -0800

    Error Handling

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const morgan = require('morgan')
+
 const app = express();
 
 const products = require("./api/routes/products");
 
+app.use(morgan("dev"));
+
 app.use("/products", products);
+
+app.use((req,res,next) => {
+    const error = new Error("Not Found!");
+    error.status = 404;
+    next(error)
+})
+
+app.use((error,req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message + error.status,
+
+        }
+    })
+
+})
 
 module.exports = app;
