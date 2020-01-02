dac6008367b2263514c5dfa61deee6b01d28b100
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-01-02 12:19:58 -0800

    Add Login Flow & Generate auth token

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const Users = require("../models/users");
 
@@ -27,6 +28,35 @@ router.post("/signup", (req, res, next) => {
       });
     }
   });
+});
+
+router.post("/login", (req, res, next) => {
+  Users.find({ email: req.body.email })
+    .then(success => {
+      if (!success) {
+        return res.status(401).json({ message: "Auth Fail" });
+      }
+
+      bcrypt.compare(req.body.password, success[0].password, (err, result) => {
+        // result == true
+        if (err) {
+          return res.status(401).json({ message: "Auth Fail" });
+        }
+        if (result) {
+          const token = jwt.sign(
+            { email: success[0].email, _id: success[0]._id },
+            "secret",
+            {
+              expiresIn: "1h"
+            }
+          );
+
+          return res.status(200).json({ message: "Auth Successful", token });
+        }
+        res.status(401).json({ message: "Auth Fail" });
+      });
+    })
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
